01944e5da6ee13cd3d3abeb3c7eabd97f98c02c4
Patosha <Patosha68@gmail.com>
2022-03-22 19:00:31 +0300

    feat: homeworks-1 completed

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <div className={styles.message}>
+            <img className={styles.message__avatar} src={`${props.avatar}`} alt={"avatar"}/>
 
+            <div className={styles.message__wrap}>
+
+                <h3 className={styles.message__name}>
+                    {props.name}
+                </h3>
+
+                <div className={styles.message__footer}>
+                    <p className={styles.message__text}>
+                        {props.message}
+                    </p>
+
+                    <span className={styles.message__time}>
+                        {props.time}
+                    </span>
+
+                </div>
+            </div>
         </div>
     )
 }
