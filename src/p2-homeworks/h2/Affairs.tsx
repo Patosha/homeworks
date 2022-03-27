83c9c7ea7523d09aabab401d4772366d42d5c465
Patosha <Patosha68@gmail.com>
2022-03-27 10:02:50 +0300

    fix: Affairs, Affair component

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -4,7 +4,7 @@ import {AffairType, FilterType} from './HW2'
 
 type AffairsPropsType = { // need to fix any
     data: Array<AffairType>
-    setFilter: (filter: FilterType) => void
+    setFilter: (type: FilterType) => void
     deleteAffairCallback: (_id: number) => void
 }
 
@@ -19,12 +19,17 @@ function Affairs(props: AffairsPropsType) {
     ))
 
     const setAll = () => {
+        props.setFilter('all')
+
     } // need to fix
     const setHigh = () => {
+        props.setFilter('high')
     }
     const setMiddle = () => {
+        props.setFilter('middle')
     }
     const setLow = () => {
+        props.setFilter('low')
     }
 
     return (
